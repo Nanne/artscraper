fa3033e589bb31030ac9e9b5d6845fa85f0639f6
Nanne <n.j.e.vannoord@uva.nl>
2022-10-07 15:31:15 +0200

    modified init file

--- artscraper/__init__.py
+++ artscraper/__init__.py
@@ -2,5 +2,9 @@
 
 from artscraper.googleart import GoogleArtScraper
 from artscraper.wikiart import WikiArtScraper
+from artscraper.philamuseum import PhiladelphiaMuseumScraper
+from artscraper.getty import GettyScraper
+from artscraper.rijksmuseum import RijksmuseumScraper
+from artscraper.artic import ArticScraper
 
-__all__ = ["GoogleArtScraper", "WikiArtScraper"]
+__all__ = ["GoogleArtScraper", "WikiArtScraper", "PhiladelphiaMuseumScraper", "GettyScraper", "RijksmuseumScraper", "ArticScraper"]
